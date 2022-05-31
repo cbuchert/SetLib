897091d5602c29c19eac155116b592b87302de53
Chris <christianbuchert@gmail.com>
2022-05-31 16:01:17 -0600

    Compare sets for equality.

---
+++ src/getSetsAreEqual.ts
@@ -0,0 +1,13 @@
+export const getSetsAreEqual = <T>(a: Set<T>, b: Set<T>): boolean => {
+  if (a.size !== b.size) {
+    return false
+  }
+
+  for (const value of a) {
+    if (!b.has(value)) {
+      return false
+    }
+  }
+
+  return true
+}
